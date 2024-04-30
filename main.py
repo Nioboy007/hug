82705ce64ad96ff517b30118a89ed1839009146e
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-30 12:14:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ API_URL = "https://api-inference.huggingface.co/models/succinctly/text2image-pro
 headers = {"Authorization": "Bearer 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"}
 
 def query(payload):
-	response = requests.post(API_URL, headers=headers, json=payload)
-	return response.json()
-	
+    response = requests.post(API_URL, headers=headers, json=payload)
+    return response.json()
+
 output = query({
-	"inputs": "a naked girl",
+    "inputs": "a naked girl",
 })
+
+print(output)
