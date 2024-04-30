d41a4c68edd0cdaa0a9bef2b4b4d0672622b2470
Nioboy007 <161853537+Nioboy007@users.noreply.github.com>
2024-04-30 12:11:22 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/succinctly/text2image-prompt-generator"
+headers = {"Authorization": "Bearer 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"}
+
+def query(payload):
+	response = requests.post(API_URL, headers=headers, json=payload)
+	return response.json()
+	
+output = query({
+	"inputs": "a naked girl",
+})
